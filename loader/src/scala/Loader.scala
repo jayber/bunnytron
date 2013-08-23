6833e1e89a74e2fb64fdbe0ad104037d6c22df15
jayber <james.a.bromley@googlemail.com>
2013-08-23 17:45:15 +0100

    add taxonomy management

--- loader/src/scala/Loader.scala
+++ loader/src/scala/Loader.scala
@@ -1,7 +1,23 @@
-import scala.xml.Elem
+import java.util
+import org.springframework.web.client.RestTemplate
+import scala.xml.{Node, NodeSeq, Elem}
 
 val document: Elem = xml.XML.loadFile(args(0))
 
-//    println(document)
-println((document \ "service" \ "practiceAreaList" \ "practiceArea" \ "name").sortBy(_.text).mkString("\n"))
+val topLevelPracticeAreas: NodeSeq = document \ "service" \ "practiceAreaList" \ "practiceArea"
 
+topLevelPracticeAreas.foreach(loadNode(_, Option("")))
+
+val rest: RestTemplate = new RestTemplate()
+
+def loadNode(node: Node, parentName: Option[String]): Unit = {
+  val map: util.HashMap[String, String] = new util.HashMap[String, String]()
+  val nodeName: String = (node \ "name").text
+  map.put("name", nodeName)
+  map.put("parentName", parentName.getOrElse(""))
+
+  println(s"/${parentName.getOrElse("")}/$nodeName")
+  //  rest.postForLocation(new URI("http://localhost:8080/"), map)
+
+  (node \ "practiceAreaList" \ "practiceArea").foreach(loadNode(_, Option(nodeName)))
+}
